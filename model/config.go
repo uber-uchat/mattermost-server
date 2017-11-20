0a0bffa914bd4770e71592f3d79861dff622a9e3
David Meza <dmeza@ext.uber.com>
2017-11-20 14:16:54 -0500

    Add config value EnableSignInWithEmailFromWebhook to be able to login from webhooks even when "EnableSignInWithEmail": false

--- model/config.go
+++ model/config.go
@@ -720,6 +720,7 @@ type EmailSettings struct {
 	EnableSignUpWithEmail             bool
 	EnableSignInWithEmail             *bool
 	EnableSignInWithUsername          *bool
+	EnableSignInWithEmailFromWebhook  *bool
 	SendEmailNotifications            bool
 	UseChannelInEmailNotifications    *bool
 	RequireEmailVerification          bool
@@ -757,6 +758,10 @@ func (s *EmailSettings) SetDefaults() {
 
 	if s.EnableSignInWithUsername == nil {
 		s.EnableSignInWithUsername = NewBool(false)
+	}
+
+	if s.EnableSignInWithEmailFromWebhook == nil {
+		s.EnableSignInWithEmailFromWebhook = NewBool(false)
 	}
 
 	if s.UseChannelInEmailNotifications == nil {
