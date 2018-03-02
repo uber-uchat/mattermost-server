c305c5451acdec3db885adfc055bee51e5f40ac6
Miguel Espinoza <espinozamiguel349@gmail.com>
2018-03-02 11:43:45 -0500

    DisableAutoResponse when manually setting status

--- api4/status.go
+++ api4/status.go
@@ -72,7 +72,7 @@ func updateUserStatus(c *Context, w http.ResponseWriter, r *http.Request) {
 	}
 
 	currentStatus, err := c.App.GetStatus(c.Params.UserId)
-	if err == nil && currentStatus.Status == model.STATUS_OUT_OF_OFFICE && status.Status == model.STATUS_ONLINE {
+	if err == nil && currentStatus.Status == model.STATUS_OUT_OF_OFFICE && status.Status != model.STATUS_OUT_OF_OFFICE {
 		c.App.DisableAutoResponse(c.Params.UserId, c.IsSystemAdmin())
 	}
 
