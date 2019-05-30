be271fa5c4b04de4f02141bf1c3337f144ee55f1
Miguel de la Cruz <miguel@mcrx.me>
2019-05-30 16:48:19 +0100

    Add Trace config option to the Elasticsearch block

--- model/config.go
+++ model/config.go
@@ -2010,6 +2010,7 @@ type ElasticsearchSettings struct {
 	LiveIndexingBatchSize         *int    `restricted:"true"`
 	BulkIndexingTimeWindowSeconds *int    `restricted:"true"`
 	RequestTimeoutSeconds         *int    `restricted:"true"`
+	Trace                         *string `restricted:"true"`
 }
 
 func (s *ElasticsearchSettings) SetDefaults() {
@@ -2087,6 +2088,10 @@ func (s *ElasticsearchSettings) SetDefaults() {
 
 	if s.RequestTimeoutSeconds == nil {
 		s.RequestTimeoutSeconds = NewInt(ELASTICSEARCH_SETTINGS_DEFAULT_REQUEST_TIMEOUT_SECONDS)
+	}
+
+	if s.Trace == nil {
+		s.Trace = NewString("")
 	}
 }
 
