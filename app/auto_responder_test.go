7f1d2ca3489b3fc728fa09679ec82c48def4b05d
codearth01 <34037001+codearth01@users.noreply.github.com>
2019-09-16 17:46:53 +0530

    [UCHAT-4451] Changes for consolidation of OOO replies (#29)

--- app/auto_responder_test.go
+++ app/auto_responder_test.go
@@ -177,3 +177,61 @@ func TestSendAutoResponseFailure(t *testing.T) {
 		assert.False(t, autoResponderPostFound)
 	}
 }
+
+func TestAutoResponseConsolidation(t *testing.T) {
+	th := Setup(t).InitBasic()
+	defer th.TearDown()
+
+	user := th.CreateUser()
+	defer th.App.PermanentDeleteUser(user)
+
+	patch := &model.UserPatch{}
+	patch.NotifyProps = make(map[string]string)
+	patch.NotifyProps["auto_responder_active"] = "true"
+	patch.NotifyProps["auto_responder_message"] = "Hello, I'm unavailable today."
+
+	userUpdated1, err := th.App.PatchUser(user.Id, patch, true)
+	require.Nil(t, err)
+	err = th.App.InsertOooRequestUser(user.Id, 1, 1, map[string]string{}, map[string]string{})
+	require.Nil(t, err)
+	th.App.SetStatusOutOfOffice(user.Id)
+
+	user = th.CreateUser()
+	defer th.App.PermanentDeleteUser(user)
+
+	patch = &model.UserPatch{}
+	userUpdated2, err := th.App.PatchUser(user.Id, patch, true)
+	require.Nil(t, err)
+	_, err = th.App.AddTeamMember(th.BasicTeam.Id, userUpdated2.Id)
+	require.Nil(t, err)
+	_, err = th.App.AddUserToChannel(userUpdated2, th.BasicChannel)
+	require.Nil(t, err)
+
+	th.App.CreatePost(&model.Post{
+		ChannelId: th.BasicChannel.Id,
+		Message:   "zz" + model.NewId() + "a",
+		UserId:    th.BasicUser.Id},
+		th.BasicChannel,
+		false)
+
+	th.App.SendAutoResponse(th.BasicChannel, userUpdated1, userUpdated2)
+
+	if _, err := th.App.GetPosts(th.BasicChannel.Id, 0, 1); err != nil {
+		require.Nil(t, err)
+	} else {
+		th.App.SendAutoResponse(th.BasicChannel, userUpdated1, userUpdated2)
+
+		if list, err := th.App.GetPosts(th.BasicChannel.Id, 0, 1); err != nil {
+			require.Nil(t, err)
+		} else {
+			autoResponderPostCount := 0
+			for _, post := range list.Posts {
+				if post.Type == model.POST_AUTO_RESPONDER {
+					autoResponderPostCount += 1
+				}
+			}
+			assert.True(t, autoResponderPostCount == 1)
+		}
+	}
+
+}
